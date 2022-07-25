5f01d72d38d97e7803dcb74fb93af0824690d094
Ma5ee2 <hussainmasee@gmail.com>
2022-07-25 12:02:57 +0100

    All the location are showing on the map

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
-import { Component } from "react";
-import { CompanyList, get_all_companies } from './components/companyData';
+import { useJsApiLoader } from '@react-google-maps/api';
+import GoogleMaps from './components/googleMap'
+
+const center = { lat: 51.5072, lng: 0.1276}
 
 function App() {
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey : process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  })
+
+  if (!isLoaded) {
+      return false;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Map</h1>
-      </header>
-      <main>
-        <h2>{ get_all_companies() }</h2>
-      </main>
+      <h1>Map</h1>
+      <div className="map-section">
+        <GoogleMaps />
+      </div>
     </div>
   );
 }
---
+++ client/src/components/googleMap.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { GoogleMap, Marker } from '@react-google-maps/api';
+import axios from "axios";
+
+const center = { lat: 51.5072, lng: 0.1276}
+
+export default class GoogleMaps extends Component {
+    state = {
+        companies: []
+    }
+
+    componentDidMount() {
+        axios.get(`https://run.mocky.io/v3/7cb595ed-2882-4dc7-8179-d38d0b9c9d13`)
+        .then(res => {
+            const companies = res.data;
+            this.setState({ companies })
+        })
+        .catch(function (error) {
+            console.log(error);
+        })   
+    };
+
+    render() {
+        return (
+            <div className="map-box">
+                <GoogleMap
+                center={center}
+                zoom={11}
+                mapContainerStyle={{height: '100%', width: '100%'}}
+                >
+                {this.state.companies.map(company => {  
+                    console.log(company.location);
+                    return (
+                        <Marker  
+                            key={company.id}
+                            position={{lat: company.location.latitude, lng: company.location.longitude}}
+                        />
+                    );
+                })}
+                </GoogleMap>
+            </div>
+        )
+    }
+}
+ 
+
